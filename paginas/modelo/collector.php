049ade55c800e51c37f8613b51e41a55bca09553
Shimberly <kimmy_11_07@hotmail.com>
2017-02-24 16:17:34 -0500

    base de datos

--- paginas/modelo/collector.php
+++ paginas/modelo/collector.php
@@ -2,10 +2,10 @@
 include_once('database.php');
 
 // Define configuration
-define("DB_HOST", "localhost");
-define("DB_USER", "postgres");
-define("DB_PASS", "postgres");
-define("DB_NAME", "postgresMineria");
+define("DB_HOST", "ec2-23-21-76-49.compute-1.amazonaws.com");
+define("DB_USER", "gsuyxmkwfctjcp");
+define("DB_PASS", "5a9e002abd32c90154420fc97d85a07af7bac637fe0a42dc0de7b8859c700cd4");
+define("DB_NAME", "d1j6p1pqvglm70");
 
 
 class Collector extends dataBase
